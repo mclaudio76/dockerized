1d25f857345adc4224a67fb58db25626ff353f87
Mauri Claudio <mauri.claudio@gmail.com>
2019-09-15 11:30:52 +0200

    Added test

--- src/test/java/com/example/demo/DockerizedApplicationTests.java
+++ src/test/java/com/example/demo/DockerizedApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -7,7 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DockerizedApplicationTests {
-
+	
+	@Test
+	public void start() {
 		
-
+	}
 }
