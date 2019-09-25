855857e8f11e4227d089aeabce4fc895f65e3b31
mclaudio76 <claudio.mauri@limontainformatica.it>
2019-09-25 17:56:13 +0200

    Changed main rest

--- src/main/java/com/example/demo/RESTHello.java
+++ src/main/java/com/example/demo/RESTHello.java
@@ -8,7 +8,7 @@ public class RESTHello {
 
 	@GetMapping(path = "/hello")
 	public Greeting helloMe() {
-		return new Greeting("Mauri Claudio");
+		return new Greeting("This is a test");
 	}
-}
+}  
 
